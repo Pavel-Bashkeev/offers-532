eb71201a58119be5023cd9e2371a29972ebc7456
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-08-12 18:51:48 +0300

    проблемы со слайдером

--- app/js/vendor/slider.js
+++ app/js/vendor/slider.js
@@ -2,5 +2,23 @@ window.addEventListener('DOMContentLoaded', () =>{
   $('.slider-inner').slick({
     arrows: false,
     dots: true,
+    infinite: false
+  });
+
+  const sliderItem = document.querySelectorAll('.slick-slide');
+  const dotsList = document.querySelector('.slick-dots');
+  sliderItem.forEach(item => {
+    let indexSlide = item.getAttribute('data-slick-index');
+    console.log(indexSlide);
+    console.log(typeof(indexSlide))
+    console.log(indexSlide === '0');
+    if(indexSlide === '0' && item.classList.contains('slick-current')){
+      console.log('eas')
+      dotsList.classList.add('first-item-slick');
+    }else{
+      dotsList.classList.remove('first-item-slick');
+      console.log('no')
+    }
   })
+  
 })
