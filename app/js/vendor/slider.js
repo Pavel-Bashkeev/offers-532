6a3c484775e6bae1fdf9bb3d2f43b75dc24c5347
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-08-12 21:45:41 +0300

    and slider

--- app/js/vendor/slider.js
+++ app/js/vendor/slider.js
@@ -1,24 +1,17 @@
-window.addEventListener('DOMContentLoaded', () =>{
-  $('.slider-inner').slick({
-    arrows: false,
-    dots: true,
-    infinite: false
-  });
 
-  const sliderItem = document.querySelectorAll('.slick-slide');
-  const dotsList = document.querySelector('.slick-dots');
-  sliderItem.forEach(item => {
-    let indexSlide = item.getAttribute('data-slick-index');
-    console.log(indexSlide);
-    console.log(typeof(indexSlide))
-    console.log(indexSlide === '0');
-    if(indexSlide === '0' && item.classList.contains('slick-current')){
-      console.log('eas')
-      dotsList.classList.add('first-item-slick');
-    }else{
-      dotsList.classList.remove('first-item-slick');
-      console.log('no')
-    }
-  })
-  
+const slider = new Swiper('.swiper-container',{
+  direction: 'horizontal',
+  autoHeight: true,
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+window.addEventListener('DOMContentLoaded', ()=>{
+  const slides =document.querySelectorAll('.swiper-slide');
+const dotsBox = document.querySelector('.swiper-pagination-fraction');
+
+slides.forEach((item, index) => {
+ 
+});
 })
